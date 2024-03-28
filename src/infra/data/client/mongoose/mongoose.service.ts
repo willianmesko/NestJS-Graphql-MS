ac44ec890004c38ef0d53c86250a59cbb1162896
willian mesko <willian.mesko@openvpn.net>
2024-03-28 16:36:13 -0300

    commit

--- src/infra/data/client/mongoose/mongoose.service.ts
+++ src/infra/data/client/mongoose/mongoose.service.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://localhost:27017/mongo-graphql'),
+      mongoose.connect(process.env.MONGO_URI),
   },
 ];
